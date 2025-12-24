5ffdd051ec750aaa62b815af5438d127973ee4e6
juanpablofernandez <juanpablofer17@gmail.com>
2025-12-23 21:39:34 -0600

    Initial Commit

---
+++ content/_meta.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  index: 'Welcome to Prosperia',
+  'getting-started': {
+    title: 'Getting Started',
+    // collapsible: false,
+  },
+  survival: 'Economy Survival',
+};
---
+++ content/getting-started/_meta.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  'connection-info': 'How to Join',
+  'global-rules': 'Server Rules',
+  'discord-rules': 'Discord Rules',
+};
